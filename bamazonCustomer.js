4afc1981f4a738f6fc1fd84c64a176810c87dd4d
Paul Palma <palmachato@yahoo.com>
2019-10-13 02:02:25 -0400

    finished js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
-
+var total;
 var inquirer = require("inquirer");
+var new_value;
 
 
 
@@ -51,5 +52,59 @@ function start() {
 
         if (err) throw err;
         console.table(results)
+
+        askUser()
 }
     )}
+
+    function askUser() {
+      inquirer.prompt ([
+        {
+        type: "input",
+        name: "whatToBuy",
+        message: "What do you want to buy? (provide the id)"
+           },
+           {
+            type: "input",
+            name: "howManyToBuy",
+            message: "How many do you want to buy?"
+               }]).then(function(answers){
+         var howManyToBuy = answers.howManyToBuy
+         howManyToBuy = parseInt(howManyToBuy)
+        
+         var whatToBuy = answers.whatToBuy
+         
+
+         findQuantity(howManyToBuy, whatToBuy)
+        });
+    }
+
+   function findQuantity(howManyToBuy,whatToBuy) {
+    connection.query('SELECT stock_quantity FROM products WHERE item_id = ' + whatToBuy, function(err, results) {
+
+      if (err) throw err;
+      
+       total = results[0].stock_quantity
+      
+      updateProducts(howManyToBuy,whatToBuy)
+})
+ 
+function updateProducts(howManyToBuy,whatToBuy) {
+ new_value = total - howManyToBuy
+ console.log("new val", new_value)
+    connection.query("UPDATE products SET stock_quantity=" + new_value + " WHERE item_id = " + whatToBuy, function(err, results) {
+
+      if (err) throw err;
+      console.table(results)
+      showTable()
+    })
+  }
+}
+
+function showTable() {
+  connection.query("SELECT * FROM products", function(err, results) {
+
+    if (err) throw err;
+    console.table(results)
+})
+}
